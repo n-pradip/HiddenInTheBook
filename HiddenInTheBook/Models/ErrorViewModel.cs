2f1e5cd34f7677cd8d1b56fa9a182601e40634fa
n-pradip <n.pradip101@gmail.com>
2023-03-19 22:56:39 +0545

    Add project files.

---
+++ HiddenInTheBook/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace HiddenInTheBook.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
