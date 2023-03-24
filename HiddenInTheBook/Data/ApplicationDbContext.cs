8eada5f9e1529c6eb13f675c67f576f55834fc2d
n-pradip <n.pradip101@gmail.com>
2023-03-24 19:17:43 +0545

    Add bootswatch theme and category page

---
+++ HiddenInTheBook/Data/ApplicationDbContext.cs
@@ -0,0 +1,14 @@
+﻿using HiddenInTheBook.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace HiddenInTheBook.Data
+{
+    public class ApplicationDbContext : DbContext 
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+            
+        }
+        public DbSet<Category> Categories { get; set; }
+    }
+}
