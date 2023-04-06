f95698f848c344999f7a8c7525bb3515fa56527e
n-pradip <n.pradip101@gmail.com>
2023-04-06 20:18:26 +0545

    feat(Product): add product model

---
+++ HiddenInTheBook.Models/Product.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HiddenInTheBook.Models
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        [Required]
+        public string Title { get; set; }
+        [Required]
+        public string Description { get; set; }
+        [Required]
+        public string ISBN { get; set; }
+        [Required]
+        public string Author { get; set; }
+        [Required]
+        [Range(1,1000)]
+        public double ListPrice { get; set; }
+        [Required]
+        [Range(1, 1000)]
+        public double Price { get; set; }
+        [Required]
+        [Range(1, 1000)]
+        public double Price50 { get; set; }
+
+        [Required]
+        [Range(1, 1000)]
+        public double Price100 { get; set; }
+
+        public string ImageUrl { get; set; }
+
+        public int CategoryId { get; set; }
+        public Category Category { get; set; }
+
+        public int CoverTypeId { get; set; }
+        public CoverType CoverType { get; set; }
+    }
+}
