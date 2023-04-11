6174db33bc97f9a8884cc5a58433c996a1815a49
n-pradip <n.pradip101@gmail.com>
2023-04-11 22:06:52 +0545

    feat(Product): add display name data annotations to category, prices and coverTypeId

--- HiddenInTheBook.Models/Product.cs
+++ HiddenInTheBook.Models/Product.cs
@@ -20,25 +20,31 @@ namespace HiddenInTheBook.Models
         [Required]
         public string Author { get; set; }
         [Required]
-        [Range(1,1000)]
+		[Display(Name = "List Price")]
+		[Range(1,1000)]
         public double ListPrice { get; set; }
         [Required]
-        [Range(1, 1000)]
+		[Display(Name = "Price for 1-50")]
+		[Range(1, 1000)]
         public double Price { get; set; }
         [Required]
-        [Range(1, 1000)]
+		[Display(Name = "Price for 51-100")]
+		[Range(1, 1000)]
         public double Price50 { get; set; }
 
         [Required]
-        [Range(1, 1000)]
+		[Display(Name = "Price for 100+")]
+		[Range(1, 1000)]
         public double Price100 { get; set; }
         [ValidateNever]
         public string ImageUrl { get; set; }
-
-        public int CategoryId { get; set; }
+        [Required]
+		[Display(Name = "Category")]
+		public int CategoryId { get; set; }
         [ValidateNever]
         public Category Category { get; set; }
-
+        [Required]
+        [Display(Name ="Cover Type")]
         public int CoverTypeId { get; set; }
         [ValidateNever]
         public CoverType CoverType { get; set; }
