1140aa077e1999c66c4dd64f008c3c3a4a9e7cca
n-pradip <n.pradip101@gmail.com>
2023-04-15 15:06:02 +0545

    fix: remove bootstrap default folder from wwwroot

--- HiddenInTheBook/Program.cs
+++ HiddenInTheBook/Program.cs
@@ -36,7 +36,7 @@ app.UseRouting();
 app.UseAuthentication();;
 
 app.UseAuthorization();
-
+app.MapRazorPages();
 app.MapControllerRoute(
     name: "default",
     pattern: "{area=Customer}/{controller=Home}/{action=Index}/{id?}");
