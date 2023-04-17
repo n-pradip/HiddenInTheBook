8b39fe742bb2638186b76bb2ada0a6b95295149c
n-pradip <n.pradip101@gmail.com>
2023-04-17 09:52:40 +0545

    feat: add application user model extending identity user

--- HiddenInTheBook.DataAccess/Data/ApplicationDbContext.cs
+++ HiddenInTheBook.DataAccess/Data/ApplicationDbContext.cs
@@ -13,6 +13,7 @@ namespace HiddenInTheBook.DataAccess.Data
         public DbSet<Category> Categories { get; set; }
         public DbSet<CoverType> CoverTypes { get; set; }
         public DbSet<Product> Products { get; set; }
+        public DbSet<ApplicationUser> ApplicationUsers { get; set; }
 
     }
 }
